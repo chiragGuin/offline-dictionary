dddd0951db55be1618565825476aeca1ea746900
chiragGuin <ashaguin3@gmail.com>
2025-03-09 09:13:20 +0530

    update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,17 @@
+import sqlite3
 
+def init_db():
+    conn = sqlite3.connect("backend/data/dictionary.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS words (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT UNIQUE NOT NULL,
+            meaning TEXT NOT NULL
+        );
+    """)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    init_db()
